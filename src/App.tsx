f66926b963fe9400f68d8e007382080580995cd2
vladimir.serbin <vladimir.serbin@truelab.games>
2026-07-08 17:24:47 +0300

    add scroll progress

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,28 @@
+import { useCallback, useEffect, useState } from "react";
+
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import Main from "./components/Main/Main";
 
 function App() {
+  const [scrollY, setScrollY] = useState(0);
+
+  const handleScroll = useCallback(() => {
+    const currentScroll = Number(window.scrollY.toFixed(0));
+    setScrollY(currentScroll);
+  }, []);
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [handleScroll]);
+
   return (
     <>
-      <Header />
+      <Header scrollY={scrollY} />
       <Main />
       <Footer />
     </>
